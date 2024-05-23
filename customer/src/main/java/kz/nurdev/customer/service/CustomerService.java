36f5e29f0aadf28138f7eca505f8a247df2218df
n-tolegenov <nurbol.tolegenovv@gmail.com>
2024-05-23 11:55:22 +0500

    Подключение к Базе Данных

--- customer/src/main/java/kz/nurdev/customer/service/CustomerService.java
+++ customer/src/main/java/kz/nurdev/customer/service/CustomerService.java
@@ -1,16 +1,20 @@
 package kz.nurdev.customer.service;
 
 import kz.nurdev.customer.model.Customer;
+import kz.nurdev.customer.repository.CustomerRepository;
 import kz.nurdev.customer.request.CustomerRegistrationRequest;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+
+        customerRepository.save(customer);
+
     }
 }
